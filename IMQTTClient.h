aa0dcbfd7827553f18abebfb388ead6c3db3fc25
daniel <dwm@afmsoftware.com>
2019-06-16 19:43:24 -0400

    Included additional structure/test class.

--- IMQTTClient.h
+++ IMQTTClient.h
@@ -8,14 +8,37 @@
 #ifndef _H_IMQTTCLIENT
 #define _H_IMQTTCLIENT
 
+#include <cstdint>
 #include <memory>
+#include <string>
+#include <vector>
+
+#include <json.hpp>
 
 namespace afm {
     namespace communications {
+        enum MQTT_QOS
+        {
+            MQTT_QOS_0 = 0,
+            MQTT_QOS_1,
+            MQTT_QOS_2,
+            END_MQTT_QOS
+        };
+
+        using MQTTBuffer = std::vector<uint8_t>;
+        using MQTTOptions = nlohmann::json;
+
         class IMQTTClient
         {
             public:
                 virtual ~IMQTTClient() {};
+
+                virtual void onConnected(bool success) = 0;
+                virtual void onSubscriptionAdded(bool success) = 0;
+                virtual void onSubscriptionRemoved(bool success) = 0;
+                virtual void onDisconnected(bool success) = 0;
+                virtual void onMessageReceived(const MQTTBuffer &topic, const MQTTBuffer &message, MQTT_QOS qos) = 0;
+                virtual void onMessageDelivered(const MQTTBuffer &topic, const MQTTBuffer &message, MQTT_QOS qos) = 0;
         };
 
         using IMQTTClientSPtr = std::shared_ptr<IMQTTClient>;
--- test/main.cpp
+++ test/main.cpp
@@ -6,9 +6,73 @@
  * AFM Software
  */
 
+#include <iostream>
+
+#include "MQTTOptions.h"
+#include "MQTTClient.h"
+
+using namespace afm;
+using namespace communications;
+
+class TestClient : public IMQTTClient, std::enable_shared_from_this<TestClient>
+{
+    public:
+        virtual void onConnected(bool success) override
+        {
+
+        }
+        virtual void onSubscriptionAdded(bool success)  override
+        {
+
+        }
+
+        virtual void onSubscriptionRemoved(bool success) override
+        {
+
+        }
+
+        virtual void onDisconnected(bool success) override
+        {
+
+        }
+
+        virtual void onMessageReceived(const MQTTBuffer &topic, const MQTTBuffer &message, MQTT_QOS qos) override
+        {
+
+        }
+
+        virtual void onMessageDelivered(const MQTTBuffer &topic, const MQTTBuffer &message, MQTT_QOS qos) override
+        {
+
+        }
+};
+
 int main(int argc, char *argv[])
 {
+    MQTTOptions mqttOptions = {
+        {sc_brokerURL, "localhost"},
+        {sc_brokerPort, 1883}
+    };
+
     int returnCode = 0;
+    std::shared_ptr<TestClient> pClient = std::make_shared<TestClient>();
+
+    MQTTClientSPtr pMQTTClient = std::make_shared<MQTTClient>();
+
+    if (pMQTTClient->initialize(mqttOptions, pClient) == true) {
+        std::cout << "Initialized\n";
+    } else {
+        std::cout << "Failed to init\n";
+    }
+
+    std::cout << "Shutting down mqttclient\n";
+    pMQTTClient->shutdown();
+
+    std::cout << "Clearing mqttclient\n";
+    
+    pMQTTClient = nullptr;
+    
+    pClient = nullptr;
 
     return returnCode;
 }
