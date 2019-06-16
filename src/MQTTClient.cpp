33ea021cb63f9a78bf6f7e455bcd818b80924e8a
daniel <dwm@afmsoftware.com>
2019-06-16 19:42:01 -0400

    Updated to include processor.

--- src/MQTTClient.cpp
+++ src/MQTTClient.cpp
@@ -16,16 +16,41 @@ namespace afm {
 
         MQTTClient::~MQTTClient()
         {
-
+            shutdown();
         }
 
-        bool MQTTClient::initialize(IMQTTClientSPtr pListener)
+        bool MQTTClient::initialize(const MQTTOptions &options, IMQTTClientSPtr pListener)
         {
             bool success = false;
 
-            m_pListener = pListener;
+            if (pListener != nullptr) {
+                m_pListener = pListener;
+                m_pProcessor = std::make_shared<MQTTProcessor>();
+
+                if (m_pProcessor->initialize(options) == true) {
+                    success = true;
+                } else {
+                    m_pProcessor->shutdown();
+                    m_pProcessor = nullptr;
+                }
+            }
+            return success;
+        }
+
+        bool MQTTClient::sendMessage(const MQTTBuffer &topic, const MQTTBuffer &message, MQTT_QOS qos)
+        {
+            bool success = false;
 
             return success;
+        }
+
+        void MQTTClient::shutdown()
+        {
+            if (m_pProcessor != nullptr) {
+                m_pProcessor->shutdown();
+                m_pProcessor = nullptr;
+            }
+            m_pListener = nullptr;
         }
     }
 }
