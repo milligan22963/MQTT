6975b7a37b9ca9073693939f9c508a6289c196fe
daniel <dwm@afmsoftware.com>
2019-06-16 19:42:59 -0400

    Added processor

--- MQTTClient.h
+++ MQTTClient.h
@@ -9,18 +9,23 @@
 #define _H_MQTTCLIENT
 
 #include "IMQTTClient.h"
+#include "MQTTProcessor.h"
 
 namespace afm {
     namespace communications {
-        class MQTTClient
+        class MQTTClient : public std::enable_shared_from_this<MQTTClient>
         {
             public:
                 MQTTClient();
                 virtual ~MQTTClient();
 
-                virtual bool initialize(IMQTTClientSPtr pListener);
+                virtual bool initialize(const MQTTOptions &options, IMQTTClientSPtr pListener);
+                virtual bool sendMessage(const MQTTBuffer &topic, const MQTTBuffer &message, MQTT_QOS qos);
+                virtual void shutdown();
+
             private:
-                IMQTTClientSPtr m_pListener = nullptr;                
+                IMQTTClientSPtr m_pListener = nullptr;
+                MQTTProcessorSPtr m_pProcessor = nullptr;
         };
 
         using MQTTClientSPtr = std::shared_ptr<MQTTClient>;
